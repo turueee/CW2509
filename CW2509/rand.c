2cca23115d7f94dc89383e6e50322ed355b92e6a
turueee <turushkine@inbox.ru>
2024-09-26 22:37:04 +0300

    'New'

--- CW2509/rand.c
+++ CW2509/rand.c
@@ -1,36 +1,14 @@
 
 #define _CRT_SECURE_NO_WARNINGS
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
 
-double ran(double min_ch, double max_ch,int sid_variable)
-{
-  double zero_one = 0, random_number = 0, zero_max_ch = 0;
-  int randomization = 0;
-  srand(time(0)+ sid_variable * 4345);
-  randomization= rand();
-  zero_one = (double)randomization/ RAND_MAX;
-  zero_max_ch= zero_one * (max_ch - min_ch);
-  random_number = zero_max_ch + min_ch;
-  return random_number;
-}
 
-int dec(double double_variable)
-{
-  double remain = 0;
-  int int_remain = 1;
-  if (double_variable >= 0)
-  {
-    remain = double_variable - floor(double_variable);
-  }
-  else
-    remain = (-double_variable) + ceil(double_variable);
-  remain *= 1000000;
-  int_remain = (int)round(remain);
-  return int_remain;
-}
+#include "func.h"
 
 
 
@@ -52,8 +30,8 @@ int main()
   scanf("%d", &count_of_numbers);
   for (counter = 0; counter < count_of_numbers; counter++)
   {
-    random_number = ran(min_ch, max_ch,counter);
-    dec_remain = dec(random_number);
+    random_number = make_random_number(min_ch, max_ch,counter);
+    dec_remain = make_dec_remain(random_number);
     if (dec_remain >= 0 && dec_remain < 250000)
     {
       first_massive_of_remains[dec_remain] = 1;
@@ -70,6 +48,6 @@ int main()
     {
       fourth_massive_of_remains[dec_remain - 750000] = 1;
     }
-    printf("%lf %d\n", random_number, dec(random_number));
+    printf("%lf %d\n", random_number, make_dec_remain(random_number));
   }
 }
