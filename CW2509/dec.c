4fc28a1ff52562c8fcb2fd88f5e53631ec5307bd
turueee <turushkine@inbox.ru>
2024-09-26 09:27:27 +0300

    'd'

--- CW2509/dec.c
+++ CW2509/dec.c
@@ -1,13 +1,29 @@
+/*
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <math.h>
 
 int dec()
 {
-  double d = 3.214526;
-    printf("%lf",d);
+  double d = -3.6, i = 0, k = 0;
+  int y = 1;
+  if (d >= 0)
+  {
+    i = d - floor(d);
+    printf("%lf\n", i);
+  }
+  else
+    i = (-d) + ceil(d);
+  i *= 1000000;
+  k = i;
+  y = (int)round(i);
+  while (y % 10 == 0)
+    y /= 10;
+  return y;
 }
 
 int main()
 {
-  dec()
+  dec();
 }
+*/
--- CW2509/rand.c
+++ CW2509/rand.c
@@ -1,33 +1,51 @@
-/*
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-int ran(double ymin, double ymax,int j)
+double ran(double ymin, double ymax,int t)
 {
   double p = 0, y = 0, k = 0;
   int i = 0,x = 0;
-  srand(time(0));
+  srand(time(0)+t*424345);
+  x = rand();
+  p = (double)x / RAND_MAX;
+  k = p * (ymax - ymin);
+  y = k + ymin;
+  return y;
+}
 
-  for (i = 0; i < j; i++)
+int dec(double d)
+{
+  double i = 0, k = 0;
+  int y = 1;
+  if (d >= 0)
   {
-    x = rand();
-    p = (double)x / RAND_MAX;
-    k = p * (ymax - ymin);
-    y = k + ymin;
-    printf("%d\t%lf\t%lf\t%lf\n", x, p, k, y);
+    i = d - floor(d);
+    printf("%lf\n", i);
   }
-  return 0;
+  else
+    i = (-d) + ceil(d);
+  i *= 1000000;
+  k = i;
+  y = (int)round(i);
+  while (y % 10 == 0)
+    y /= 10;
+  return y;
 }
 
 int main()
 {
-  double ymin = 0, ymax = 0;
-  int n = 0;
+  double y = 0,ymin = 0, ymax = 0;
+  int n = 0,i = 0;
   scanf("%lf", &ymin);
   scanf("%lf", &ymax);
   scanf("%d", &n);
-  ran(ymin,ymax,n);
+  for (i = 0; i < n; i++)
+  {
+    y = ran(ymin, ymax,i);
+    printf("%lf %d\n", y, dec(y));
+  }
 }
-*/
